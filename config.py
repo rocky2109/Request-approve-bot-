a4e3f6284ca030eac4389fddd5400d784d8a57e4
rocky2109 <rockys.2109rs@gmail.com>
2025-05-15 14:37:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "autoacceptbot")
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
 IS_FSUB = bool(environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+AUTH_CHANNELS = list(map(int, environ.get("AUTH_CHANNEL", "").split())) # Add Multiple channel id
